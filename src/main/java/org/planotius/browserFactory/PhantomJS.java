5072f760735b76fc0f121fe055b175d2d9ba397a
Gustavo Labbate Godoy <gustavolabbate@gmail.com>
2016-10-05 15:41:44 -0300

    add phantomjs driver

--- src/main/java/org/planotius/browserFactory/PhantomJS.java
+++ src/main/java/org/planotius/browserFactory/PhantomJS.java
@@ -46,44 +46,5 @@ public class PhantomJS extends BrowserDecorator {
         }
 
         return capability;
-
-        /*FirefoxProfile profile = new FirefoxProfile();
-
-         if (Config.getFirefoxProfile() != null) {
-         ProfilesIni profIni = new ProfilesIni();
-         profile = profIni.getProfile(Config.getFirefoxProfile());
-
-         }
-
-         DesiredCapabilities capability = DesiredCapabilities.firefox();
-         capability.setBrowserName(FF_BROWSER);
-         capability.setCapability(CapabilityType.TAKES_SCREENSHOT, true);
-         capability.setCapability("marionette", true);
-         System.setProperty("marionette.logging", "OFF");
-         capability.setPlatform(org.openqa.selenium.Platform.ANY);
-
-         profile.setPreference("intl.accept_languages", Config.getFirefoxLocale());
-         profile.setPreference("xpinstall.signatures.required", false);
-         */
-        /*  Info about firefox profile
-         https://groups.google.com/forum/#!topic/selenium-users/Zd5WYVZFXU0
-         */
-        /*
-         try {
-         capability.setCapability("firefox_profile", profile.toJson());
-         } catch (IOException ex) {
-         LOG.error(ex.getMessage(), ex);
-         }
-
-         if (System.getProperty("os.name").toLowerCase().contains("win")) {
-         LOG.info("WIN os recognized. Loading geckodriver from " + FF_WIN_HOME);
-         capability.setCapability("binary", FF_WIN_HOME);
-         System.setProperty("webdriver.gecko.driver", FF_WIN_HOME);
-         } else {
-         LOG.info("LINUX os recognized. Loading geckodriver from " + FF_LIN_HOME);
-         capability.setCapability("binary", FF_LIN_HOME);
-         System.setProperty("webdriver.gecko.driver", FF_LIN_HOME);
-         }
-         */
     }
 }
