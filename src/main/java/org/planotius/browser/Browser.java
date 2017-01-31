c4e26a5cd3b4436369867ba61cf83a5da345d906
Gustavo Godoy <ggodoy@lenovo.com>
2017-01-31 16:36:02 -0200

    Fix some sonarqube bugs

--- src/main/java/org/planotius/browser/Browser.java
+++ src/main/java/org/planotius/browser/Browser.java
@@ -8,15 +8,42 @@ import org.openqa.selenium.remote.DesiredCapabilities;
  * @author ggodoy
  */
 public interface Browser {
-    
-    public String getBrowserName();
-    
-    public String getBrowserLocation();
-    
-    public WebDriver getWebDriver();
-    
-    public WebDriver getRemoteWebDriver(String testServer, String port);
-    
+
+    /**
+     * Get the Browsers Name
+     *
+     * @return String
+     */
+    String getBrowserName();
+
+    /**
+     * get where the binary from browser is installed on system
+     *
+     * @return String
+     */
+    String getBrowserLocation();
+
+    /**
+     * Get the webDriver instance
+     *
+     * @return WebDriver
+     */
+    WebDriver getWebDriver();
+
+    /**
+     * Get the webDriver for remote connections
+     *
+     * @param testServer
+     * @param port
+     * @return
+     */
+    WebDriver getRemoteWebDriver(String testServer, String port);
+
+    /**
+     * Here is where you define your capabilities...
+     *
+     * @return DesiredCapabilities
+     */
     DesiredCapabilities defineCapabilities();
-    
+
 }
