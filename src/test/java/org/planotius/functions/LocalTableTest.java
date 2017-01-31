7c0b315de8a0a4f4676efca5dd24223453f3218c
Gustavo Godoy <ggodoy@lenovo.com>
2017-01-31 17:18:04 -0200

    Fix some sonarqube bugs

--- src/test/java/org/planotius/functions/LocalTableTest.java
+++ src/test/java/org/planotius/functions/LocalTableTest.java
@@ -6,7 +6,7 @@ import org.junit.AfterClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.junit.BeforeClass;
-import org.planotius.helper.Config;
+import org.planotius.helper.*;
 
 /**
  *
@@ -14,12 +14,11 @@ import org.planotius.helper.Config;
  */
 public class LocalTableTest {
 
-    static LocalTable table;
+    static LocalTable table = new LocalTable().init();;
     static Controller controller = new Controller();
 
     @BeforeClass
     public static void setup() {
-        table = new LocalTable().init();
         String url = System.getProperty("user.dir") + "\\src\\test\\resources\\localTable.html";
 
         if (Config.getBrowser().equals("firefox")) {
