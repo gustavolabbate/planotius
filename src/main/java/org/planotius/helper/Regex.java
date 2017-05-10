36fc2424a809b1dd1e3dfdbcf96033621d1aa30d
Pedro Andrade <pandrade1@lenovo.com>
2017-05-10 14:28:40 -0300

    Add Regex method to get drivers count

--- src/main/java/org/planotius/helper/Regex.java
+++ src/main/java/org/planotius/helper/Regex.java
@@ -1,7 +1,28 @@
 package org.planotius.helper;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
- * Created by Pedro on 5/10/2017.
+ * Created by pandrade1
  */
 public class Regex {
+
+    /**
+     * get drivers count on storage's inventory
+     * @param value
+     * @param pattern
+     * @param group
+     * @return int
+     */
+    public int getDrives(String value, String pattern, int group) {
+        Pattern p = Pattern.compile(pattern);
+        Matcher m = p.matcher(value);
+        if(m.find()) {
+            value = m.group(group);
+            value = value.replaceAll("\\(", "").replaceAll("\\)","");
+        }
+        return Integer.parseInt(value);
+    }
+
 }
