11d3a929d07b24b96a73074cef2bf8843ff9b373
Gustavo Godoy <ggodoy@lenovo.com>
2017-05-10 16:39:49 -0300

    Add UnitTest for Regex and Add new method for Regex

--- src/main/java/org/planotius/helper/Regex.java
+++ src/main/java/org/planotius/helper/Regex.java
@@ -17,11 +17,30 @@ public class Regex {
     public String getGroupByIndex(String value, String pattern, int group) {
         Pattern p = Pattern.compile(pattern);
         Matcher m = p.matcher(value);
-        if(m.find()) {
+        if (m.find()) {
             value = m.group(group);
-            value = value.replaceAll("\\(", "").replaceAll("\\)","");
+            value = value.replaceAll("\\(", "").replaceAll("\\)", "");
         }
         return value;
+    }
+
+    /*
+     * return a string from value using regular expression, without group Index
+     * When the pattern and String didn´t match, the result message is:
+     * "Value didn´t match!"
+     * So be wise to validate the correct return! :)
+    
+     * @param value
+     * @param pattern
+     * @param group
+     */
+    public String getMatch(String value, String pattern) {
+        Pattern p = Pattern.compile(pattern);
+        Matcher m = p.matcher(value);
+        if (m.find()) {
+            return m.group();
+        }
+        return "Value didn´t match!";
     }
 
 }
---
+++ src/test/java/org/planotius/functions/helper/RegexTest.java
@@ -0,0 +1,39 @@
+package org.planotius.functions.helper;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.planotius.helper.Regex;
+
+/**
+ *
+ * @author ggodoy
+ */
+public class RegexTest {
+
+    @Test
+    public void shouldMatch() {
+        Regex regex = new Regex();
+
+        assertEquals("123",
+                regex.getGroupByIndex("AbcDef123GH", "[0-9]+", 0));
+    }
+
+    @Test
+    public void shouldMatchWithoutGroup() {
+        Regex regex = new Regex();
+
+        assertEquals("123",
+                regex.getMatch("AbcDef123GH", "[0-9]+"));
+    }
+
+    @Test
+    public void shouldDidntMatch() {
+        Regex regex = new Regex();
+
+        assertEquals("Value didn´t match!",
+                regex.getMatch("AbcDef123GH", "[X]"));
+        
+        
+    }
+
+}
