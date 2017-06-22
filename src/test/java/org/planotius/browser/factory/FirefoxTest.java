81f61acbb2d63b0f97373e31d7acc1f9856a42f3
Gustavo Labbate Godoy <gustavolabbate@gmail.com>
2017-06-22 14:39:46 -0300

    Try to fix sonar bugs

--- src/test/java/org/planotius/browser/factory/FirefoxTest.java
+++ src/test/java/org/planotius/browser/factory/FirefoxTest.java
@@ -14,16 +14,13 @@ import org.openqa.selenium.remote.DesiredCapabilities;
  *
  * @author gustavolabbate
  */
-public class FirefoxTest {
-    private static final Logger LOG = Logger.getLogger(FirefoxTest.class.getName());
+public class FirefoxTest extends Config {
 
-    private static Config config = new Config();
-    
+    private static final Logger LOG = Logger.getLogger(FirefoxTest.class.getName());
 
     @Test
     public void setFirefoxHomeLocationCustom() {
         System.setProperty("firefox.home", "C:\\myBrowserDir\\firefox.exe");
-        config = new Config();
         BrowserFactory browserFactory = new BrowserFactory();
         Browser browser = browserFactory.getBrowser("firefox");
         browser.defineCapabilities();
@@ -57,33 +54,28 @@ public class FirefoxTest {
     @Test
     public void shouldLoadDefaultProfile() {
         System.setProperty("firefox.profile", "inexistentProfile");
-        config = new Config();
         BrowserFactory browserFactory = new BrowserFactory();
         Browser browser = browserFactory.getBrowser("firefox");
         browser.defineCapabilities();
     }
-    
+
     @Test
     public void shouldLoadCustomProfilefromFile() {
         System.setProperty("firefox.profile.path", "src\\test\\resources\\FirefoxProfile\\7lmpxvfi.QAautomation");
         System.setProperty("firefox.profile", "QAautomation");
-        config = new Config();
-        config.setFirefoxProfile(null);
+        Config.setFirefoxProfile(null);
         BrowserFactory browserFactory = new BrowserFactory();
         Browser browser = browserFactory.getBrowser("firefox");
         DesiredCapabilities desiredCapabilities = browser.defineCapabilities();
     }
-    
+
     @Test
     public void shouldLoadCustomProfile() {
         System.setProperty("firefox.profile", "QAautomation");
-        config = new Config();
         BrowserFactory browserFactory = new BrowserFactory();
         Browser browser = browserFactory.getBrowser("firefox");
         browser.defineCapabilities();
     }
-    
-    
 
     @AfterClass
     public static void tearDown() {
